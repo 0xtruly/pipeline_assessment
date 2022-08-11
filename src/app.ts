5c937bf11d406229174be40e1497be8ed834f9c8
dekunleroy <adebanjosegun589@gmail.com>
2022-08-11 12:34:54 +0100

    little fix

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,3 @@
-import axios from "axios";
-
-
 type ResultData = {
   id: string;
   gender: string;
@@ -25,22 +22,16 @@ let pageView: HTMLElement | any = document.querySelector(
 let currentPage: number = 1;
 
 const paginate = () => {
-  previousBtn.disabled = true;
-  previousBtn.style.color = "black";
-  previousBtn.style.backgroundColor = "gray";
-  pageView.innerHTML = `Page: ${currentPage}`;
   previousBtn.addEventListener("click", async () => {
     if (currentPage === 1) {
       return currentPage;
     } else {
       currentPage--;
     }
-    pageView.innerHTML = `Page: ${currentPage}`;
     await loadData(currentPage);
   });
   nextBtn.addEventListener("click", async () => {
     currentPage++;
-    pageView.innerHTML = `Page: ${currentPage}`;
     await loadData(currentPage);
   });
 };
@@ -54,11 +45,18 @@ const loadData = async (page: number = 1) => {
     const resData = await response.json();
     const paging: PagingData = resData?.results[0].paging;
     const data: ResultData[] = resData?.results[0][`${currentPage}`];
+    const pageNo = resData?.info.page;
+    pageView.innerHTML = `Page: ${pageNo}`;
     if (paging.previous) {
       previousBtn.disabled = false;
-      previousBtn.style.color = "#04AA6D";
+      previousBtn.style.color = "#fff";
+      previousBtn.style.backgroundColor = "#04AA6D";
+    } else {
+      previousBtn.disabled = true;
+      previousBtn.style.color = "black";
+      previousBtn.style.backgroundColor = "gray";
     }
-
+    console.log('resData', resData)
     for (const n in data) {
       const index = Number(n) + 1;
       const item = data[n];
@@ -66,13 +64,13 @@ const loadData = async (page: number = 1) => {
       tR?.setAttribute("data-entryid", item.id);
       const firstChild = document.querySelector(
         `tbody > tr:nth-child(${index}) > td:nth-child(1)`
-        );
+      );
       const secondChild = document.querySelector(
         `tbody > tr:nth-child(${index}) > td:nth-child(2)`
-        );
+      );
       const thirdChild = document.querySelector(
         `tbody > tr:nth-child(${index}) > td:nth-child(3)`
-        );
+      );
       firstChild.innerHTML = item.row;
       secondChild.innerHTML = item.gender;
       thirdChild.innerHTML = item.age;
@@ -82,8 +80,8 @@ const loadData = async (page: number = 1) => {
 };
 
 const startApp = async () => {
-  paginate();
   await loadData();
+  paginate();
 };
 
 document.addEventListener('DOMContentLoaded', startApp);
