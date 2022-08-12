83b3884f8f30b102fd5a35c8e752645112c337c4
dekunleroy <adebanjosegun589@gmail.com>
2022-08-12 23:05:45 +0100

    Use querySelctorAll to access table rows instead of using querySelector replace innerHTML with textContent for better performance, as innerHTML parses content as HTML which is slower than plain text

---
+++ DETAILS.md
@@ -0,0 +1,26 @@
+# Implementation details
+
+## Page numbering
+
+* Created a page number variable called `currentPage` and set its value to 1
+* Accessed the `next` and `previous` buttons and assigned them to variables called `nextBtn` and `previousBtn`
+* Added an `click` event listener to the `nextBtn` and `previousBtn` variables and added an event handler to each button that increments or decrements the `currentPage` variable by 1
+
+
+
+## API Request
+
+* Created a function called `loadData` that takes a parameter called `page` which is number corresponding to the `currentPage` value at a given time.
+* In the `loadData` function I created a `fetch` request to the `https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84&page=${`page`}` endpoint and assigned the response to a variable called `response`.
+* For a `response` with status code `200`, I used the `json` method to parse the response into a JavaScript object and assigned the result to a variable called `resData`.
+* The `resData` variable is then used to access the `results` property of the object. The `results` property is an array of objects containing the data for each page `N and N+1` and a paging property. The paging property contains a `next` and `previous` property.
+* Using the angle bracket notation I am able to access the first item in the data object by passing in the currentPage value. This is then assigned to a variable called `data`.
+
+
+## Implementation details
+
+* Using the page value from an info property in `resData` I update the `textContent` of the `pageView`. `pageView` is a label element that shows page number.
+* If we have a `previous` property in paging I disable the `previousBtn` element by setting `disabled` to true and false if otherwise.
+* Using the querySelectorAll method I select all the table row element. This returns a NodeList of all the elements that match the selector.
+* Using a for-in loop I iterate through the `data` array and and set a `data-entryid` attribute with dataItem id as the value to each table row element on the HTML table body.
+* Lastly I access each child element in the table row and set the `textContent` of each element to the value of the corresponding property in the data object.
--- src/app.ts
+++ src/app.ts
@@ -11,17 +11,51 @@ type PagingData = {
 };
 
 let previousBtn: HTMLElement | any = document.querySelector(
-"button[data-prevbtn]"
+  "button[data-prevbtn]"
 );
 let nextBtn: HTMLElement | any = document.querySelector(
-"button[data-nextbtn]"
+  "button[data-nextbtn]"
 );
 let pageView: HTMLElement | any = document.querySelector(
-"label[data-pageview]"
+  "label[data-pageview]"
 );
 let currentPage: number = 1;
 
-const paginate = () => {
+const loadData = async (page: number = 1) => {
+  const response = await fetch(
+    `https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84&page=${page}`
+  );
+
+  if (response.status === 200) {
+    const resData = await response.json();
+    const paging: PagingData = resData?.results[0].paging;
+    const data: ResultData[] = resData?.results[0][`${currentPage}`];
+    const pageNo = resData?.info.page;
+    pageView.textContent = `Page: ${pageNo}`;
+    if (paging.previous) {
+      previousBtn.disabled = false;
+    } else {
+      previousBtn.disabled = true;
+    }
+
+    const tableRow = document.querySelectorAll(`tbody[data-sink] > tr`);
+    console.log('tableRow', tableRow[0].children)
+    for (const n in data) {
+      const index = Number(n);
+      const item = data[n];
+      tableRow[index].setAttribute("data-entryid", item.id);
+      const firstChild: HTMLElement | any = tableRow[index].children[0];
+      const secondChild: HTMLElement | any = tableRow[index].children[1];
+      const thirdChild: HTMLElement | any = tableRow[index].children[2];
+      firstChild.textContent = item.row;
+      secondChild.textContent = item.gender;
+      thirdChild.textContent = item.age;
+    }
+  }
+};
+
+const startApp = async () => {
+  await loadData();
   previousBtn.addEventListener("click", async () => {
     if (currentPage === 1) {
       return currentPage;
@@ -36,52 +70,4 @@ const paginate = () => {
   });
 };
 
-const loadData = async (page: number = 1) => {
-  const response = await fetch(
-    `https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84&page=${page}`
-  );
-
-  if (response.status === 200) {
-    const resData = await response.json();
-    const paging: PagingData = resData?.results[0].paging;
-    const data: ResultData[] = resData?.results[0][`${currentPage}`];
-    const pageNo = resData?.info.page;
-    pageView.innerHTML = `Page: ${pageNo}`;
-    if (paging.previous) {
-      previousBtn.disabled = false;
-      previousBtn.style.color = "#fff";
-      previousBtn.style.backgroundColor = "#04AA6D";
-    } else {
-      previousBtn.disabled = true;
-      previousBtn.style.color = "black";
-      previousBtn.style.backgroundColor = "gray";
-    }
-    console.log('resData', resData)
-    for (const n in data) {
-      const index = Number(n) + 1;
-      const item = data[n];
-      const tR = document.querySelector(`tbody > tr:nth-child(${index})`);
-      tR?.setAttribute("data-entryid", item.id);
-      const firstChild = document.querySelector(
-        `tbody > tr:nth-child(${index}) > td:nth-child(1)`
-      );
-      const secondChild = document.querySelector(
-        `tbody > tr:nth-child(${index}) > td:nth-child(2)`
-      );
-      const thirdChild = document.querySelector(
-        `tbody > tr:nth-child(${index}) > td:nth-child(3)`
-      );
-      firstChild.innerHTML = item.row;
-      secondChild.innerHTML = item.gender;
-      thirdChild.innerHTML = item.age;
-
-    }
-  }
-};
-
-const startApp = async () => {
-  await loadData();
-  paginate();
-};
-
-document.addEventListener('DOMContentLoaded', startApp);
+document.addEventListener("DOMContentLoaded", startApp);
