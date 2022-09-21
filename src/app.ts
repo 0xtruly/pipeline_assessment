f2f8f63f29032d96067a3a3fa7c5f50c9fa1c0d5
dekunleroy <adebanjosegun589@gmail.com>
2022-09-21 01:38:25 +0100

    little fix

--- src/app.ts
+++ src/app.ts
@@ -19,12 +19,16 @@ let nextBtn: HTMLElement | any = document.querySelector(
 let pageView: HTMLElement | any = document.querySelector(
   "label[data-pageview]"
 );
+const tableBody: HTMLElement | any = document.querySelector(
+  `[data-sink]`
+);
 const tableRow: HTMLElement | any = document.querySelectorAll(
   `tbody[data-sink] > tr`
 );
 let currentPage: number = 1;
 
 const loadData = async (page: number = 1) => {
+  let newNode;
   const response = await fetch(
     `https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84&page=${page}`
   );
@@ -41,17 +45,29 @@ const loadData = async (page: number = 1) => {
       previousBtn.disabled = true;
     }
 
-    for (const n in data) {
-      const index = Number(n);
-      const item = data[n];
-      tableRow[index].setAttribute("data-entryid", item.id);
-      const firstChild: HTMLElement | any = tableRow[index].children[0];
-      const secondChild: HTMLElement | any = tableRow[index].children[1];
-      const thirdChild: HTMLElement | any = tableRow[index].children[2];
-      firstChild.textContent = item.row;
-      secondChild.textContent = item.gender;
-      thirdChild.textContent = item.age;
-    }
+    // for (const n in data) {
+    //   const index = Number(n);
+    //   const item = data[n];
+    //   tableRow[index].setAttribute("data-entryid", item.id);
+    //   const firstChild: HTMLElement | any = tableRow[index].children[0];
+    //   const secondChild: HTMLElement | any = tableRow[index].children[1];
+    //   const thirdChild: HTMLElement | any = tableRow[index].children[2];
+    //   firstChild.textContent = item.row;
+    //   secondChild.textContent = item.gender;
+    //   thirdChild.textContent = item.age;
+    // }
+    newNode = data && data.map((item: ResultData) => {
+      const { id, row, gender, age } = item;
+      return `
+    <tr data-entryid="${id}">
+    <td>${row}</td>
+    <td>${age}</td>
+    <td>${gender}</td>
+    </tr>
+    `
+    });
+    console.log('newNode', newNode.join("").toString());
+    tableBody.innerHTML = newNode.join("").toString()
   }
 };
 
